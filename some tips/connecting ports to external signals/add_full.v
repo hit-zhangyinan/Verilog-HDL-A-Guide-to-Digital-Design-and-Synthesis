1158b03e3dc9e20a93f95bba1415e7d9cff8f008
hit-zhangyinan <z2352173391@outlook.com>
2022-03-04 19:32:43 +0800

    Add files via upload

---
+++ some tips/connecting ports to external signals/add_full.v
@@ -0,0 +1,9 @@
+
+// This is a 1-bit full adder
+module add_full(A, B, C, Carry, S);
+    input A, B, C;
+    output Carry, S;
+    assign S = A^B^C;
+    assign Carry = (A&B) | (B&C) | (A&C);
+endmodule
+
