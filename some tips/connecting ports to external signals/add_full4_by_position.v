fe283838ad74bd742754d8e482d24f11b25e6e26
hit-zhangyinan <z2352173391@outlook.com>
2022-03-25 19:08:03 +0800

    Update add_full4_by_position.v

--- some tips/connecting ports to external signals/add_full4_by_position.v
+++ some tips/connecting ports to external signals/add_full4_by_position.v
@@ -6,7 +6,7 @@ module add_full4_by_position(A, B, C, S);
     output [4:0] C;
     assign C[0] = 0;
     
-    // "son module name" + "a user-defined name" + ( Current module ports' names )
+    // "son module name" + "a user-defined name" + ( Current module ports' names or internal signals )
     add_full u1(A[0], B[0], C[0], C[1], S[0]);
     add_full u2(A[1], B[1], C[1], C[2], S[1]);
     add_full u3(A[2], B[2], C[2], C[3], S[2]);
